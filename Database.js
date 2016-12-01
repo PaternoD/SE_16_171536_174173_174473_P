9c8cfd101e29115e28364ddaa47694904055d92b
Simone Rigon <simonerigon@MacBook-Pro-di-Simone.local>
2016-12-01 12:06:48 +0100

    Aggiunto Create Table

---
+++ Database.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var app = express();
+
+var pg = require('pg');
+
+var util = require('util');
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/create/', function(request, response){
+    
+    var text = 'response:';
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    
+    console.log("called databse");
+    
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+        
+        console.log("connect to database");
+        
+        //create table
+        client.query('create table utenti(
+                     id integer not null,
+                     id_nome varchar(20), 
+                     id_cognome varchar(20),
+                     id_giorno integer,
+                     id_mese integer,
+                     id_anno integer,
+                     id_indirizzo varchar(20),
+                     id_cap integer,
+                     id_comune varchar(20),
+                     id_prov varchar(3),
+                     id_tel integer,
+                     id_email varchar(20),
+                     id_peso integer,
+                     id_altezza integer,
+                     id_sesso varchar(2))', function(err, result){
+                     done();
+        
+            if(err){
+                console.error(err);
+                response.send("Errore " + err);
+            }else{
+                response.send("Table created");
+            }
+    });
+        
+    })
+})
