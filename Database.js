b50356377abe4828cd451144ed4b827cd346ef3c
Simone Rigon <simonerigon@MacBook-Pro-di-Simone.local>
2016-12-01 12:11:32 +0100

    Aggiornato Create Table

--- Database.js
+++ Database.js
@@ -20,23 +20,8 @@ app.get('/create/', function(request, response){
         console.log("connect to database");
         
         //create table
-        client.query('create table utenti(
-                     id integer not null,
-                     id_nome varchar(20), 
-                     id_cognome varchar(20),
-                     id_giorno integer,
-                     id_mese integer,
-                     id_anno integer,
-                     id_indirizzo varchar(20),
-                     id_cap integer,
-                     id_comune varchar(20),
-                     id_prov varchar(3),
-                     id_tel integer,
-                     id_email varchar(20),
-                     id_peso integer,
-                     id_altezza integer,
-                     id_sesso varchar(2))', function(err, result){
-                     done();
+        client.query('create table utenti(id integer not null, id_nome varchar(20), id_cognome varchar(20), id_giorno integer, id_mese integer, id_anno integer, id_indirizzo varchar(20), id_cap integer, id_comune varchar(20), id_prov varchar(3), id_tel integer, id_email varchar(20), id_peso integer, id_altezza integer, id_sesso varchar(2))', function(err, result){
+            done();
         
             if(err){
                 console.error(err);
@@ -48,3 +33,7 @@ app.get('/create/', function(request, response){
         
     })
 })
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
